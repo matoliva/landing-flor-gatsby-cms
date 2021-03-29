dc0335c34a06c76220c6e53322941607e72303b1
cristian.pache <cristian.pache@leniolabs.com>
2021-03-29 19:06:44 -0300

    feat: pages:: create index page

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/Layout";
+
+const Home = () => (
+  <Layout>
+    <h1>HOME PAGE</h1>
+  </Layout>
+);
+
+export default Home;
