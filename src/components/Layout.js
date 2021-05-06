0b9c2326e12dc51c037aa323d438a4a70bcea006
crpache <crpache@gmail.com>
2021-05-06 17:06:03 -0300

    feat: projects grid

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -33,7 +33,10 @@ const LayoutWrapper = styled.div`
 `
 
 const Main = styled.main`
+  display: flex;
+  flex-direction: column;
   flex: 1;
+  align-items: center;
 `
 
 const Layout = ({ children }) => (
