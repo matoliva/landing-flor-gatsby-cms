4e405b7f15611ac811ab417460a4ec02d84c148e
Matias Oliva <olivamati@gmail.com>
2021-08-10 17:37:59 -0300

    feat:: add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -111,7 +111,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/img/favicon.png`, // This path is relative to the root of the site.
+        icon: `src/img/favicon2.png`, // This path is relative to the root of the site.
       }
     },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
